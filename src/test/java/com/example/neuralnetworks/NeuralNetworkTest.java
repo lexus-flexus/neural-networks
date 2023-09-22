aa647a7e4a57640bacf764715754a7355d707305
lexus-flexus <tovstoganov.ak@gmail.com>
2023-09-22 14:47:28 +0300

    add test fit_binary

--- src/test/java/com/example/neuralnetworks/NeuralNetworkTest.java
+++ src/test/java/com/example/neuralnetworks/NeuralNetworkTest.java
@@ -8,7 +8,7 @@ import java.util.List;
 class NeuralNetworkTest {
 
     @Test
-    void fit() {
+    void fit_bipolar() {
 
         int x[][] = new int[][]{
                 {1, 1, -1, 1, 1, 1, 1, -1, -1, 1},
@@ -20,6 +20,21 @@ class NeuralNetworkTest {
         List<Neuron> neurons = neuralNetwork.getNeuronList();
         int[] actual = neurons.get(0).getWeight();
         int[] expected = new int[]{0, 0, -2, 0, 0, 2, 0, -2, 0, 0};
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    void fit_binary() {
+
+        int x[][] = new int[][]{
+                {1, 1, 0, 1, 1, 1, 1, 0, 0, 1},
+                {1, 1, 1, 1, 1, 0, 1, 1, 0, 1}
+        };
+        int y[] = new int[]{1, -1};
+        NeuralNetwork neuralNetwork = new NeuralNetwork(1, x[0].length, SignalRepresentation.BINARY);
+        neuralNetwork.fit(x, y);
+        List<Neuron> neurons = neuralNetwork.getNeuronList();
+        int[] actual = neurons.get(0).getWeight();
+        int[] expected = new int[]{0, 0, -1, 0, 0, 1, 0, -1, 0, 0};
         Assertions.assertArrayEquals(expected, actual);
     }
 }
