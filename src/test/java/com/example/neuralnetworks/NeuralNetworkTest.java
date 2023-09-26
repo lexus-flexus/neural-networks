5d9120e3ce86f019db2d9037d5c1d7b79ea5b665
lexus-flexus <tovstoganov.ak@gmail.com>
2023-09-26 13:08:21 +0300

    add test predict

--- src/test/java/com/example/neuralnetworks/NeuralNetworkTest.java
+++ src/test/java/com/example/neuralnetworks/NeuralNetworkTest.java
@@ -22,6 +22,7 @@ class NeuralNetworkTest {
         int[] expected = new int[]{0, 0, -2, 0, 0, 2, 0, -2, 0, 0};
         Assertions.assertArrayEquals(expected, actual);
     }
+
     @Test
     void fit_BINARY() {
 
@@ -29,12 +30,41 @@ class NeuralNetworkTest {
                 {1, 1, 0, 1, 1, 1, 1, 0, 0, 1},
                 {1, 1, 1, 1, 1, 0, 1, 1, 0, 1}
         };
-        int y[] = new int[]{1, -1};
+        int y[] = new int[]{1, 0};
         NeuralNetwork neuralNetwork = new NeuralNetwork(1, x[0].length, SignalRepresentation.BINARY);
         neuralNetwork.fit(x, y);
         List<Neuron> neurons = neuralNetwork.getNeuronList();
         int[] actual = neurons.get(0).getWeight();
         int[] expected = new int[]{0, 0, -1, 0, 0, 1, 0, -1, 0, 0};
         Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void predict_BINARY() {
+
+        int x[][] = new int[][]{
+                {1, 1, 0, 1, 1, 1, 1, 0, 0, 1},
+                {1, 1, 1, 1, 1, 0, 1, 1, 0, 1}
+        };
+        int y[] = new int[]{1, 0};
+        NeuralNetwork neuralNetwork = new NeuralNetwork(1, x[0].length, SignalRepresentation.BINARY);
+        neuralNetwork.fit(x, y);
+        Assertions.assertEquals(0, neuralNetwork.predict(new int[]{1, 1, 1, 1, 0, 0, 0, 1, 1, 1}));
+        Assertions.assertEquals(1, neuralNetwork.predict(new int[]{1, 0, 0, 0, 0, 1, 0, 0, 0, 0}));
+    }
+
+    @Test
+    void predict_BIPOLAR() {
+
+        int x[][] = new int[][]{
+                {1, 1, -1, 1, 1, 1, 1, -1, -1, 1},
+                {1, 1, 1, 1, 1, -1, 1, 1, -1, 1}
+        };
+        int y[] = new int[]{1, -1};
+        NeuralNetwork neuralNetwork = new NeuralNetwork(1, x[0].length, SignalRepresentation.BIPOLAR);
+        neuralNetwork.fit(x, y);
+        Assertions.assertEquals(1, neuralNetwork.predict(new int[]{1, -1, -1, -1, -1, 1, -1, -1, -1, -1}));
+        Assertions.assertEquals(-1, neuralNetwork.predict(new int[]{1, 1, 1, 1, -1, -1, -1, 1, 1, 1}));
+
     }
 }
