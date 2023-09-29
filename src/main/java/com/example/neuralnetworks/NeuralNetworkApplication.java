2cbb4856a72d6c97a92d91d55ff009980d79fced
lexus-flexus <tovstoganov.ak@gmail.com>
2023-09-29 13:39:34 +0300

    add components in view hello-view

--- src/main/java/com/example/neuralnetworks/NeuralNetworkApplication.java
+++ src/main/java/com/example/neuralnetworks/NeuralNetworkApplication.java
@@ -1,20 +1,20 @@
 package com.example.neuralnetworks;
 
-import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class NeuralNetworkApplication extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(NeuralNetworkApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+
     }
 
     public static void main(String[] args) {
