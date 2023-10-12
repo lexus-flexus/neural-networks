672c71b91d75b985c8884b2914edaaafa894bf9f
lexus-flexus <tovstoganov.ak@gmail.com>
2023-10-12 15:12:51 +0300

    Add resizing of rectangles depending on the size of the grid in Controller

--- src/main/java/com/example/neuralnetworks/Controller.java
+++ src/main/java/com/example/neuralnetworks/Controller.java
@@ -2,13 +2,10 @@ package com.example.neuralnetworks;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
@@ -17,13 +14,6 @@ import java.util.Arrays;
 
 public class Controller {
 
-//    @FXML
-//    private Label welcomeText;
-//
-//    @FXML
-//    protected void onHelloButtonClick() {
-//        welcomeText.setText("Welcome to JavaFX Application!");
-//      }
     @FXML
     private ChoiceBox<Integer> choiceBox;
     @FXML
@@ -33,16 +23,18 @@ public class Controller {
     @FXML
     private Button myButton;
     private int[][] gridData;
-
+    private final double gridWidth = 300.0;
+    private final double gridHeight = 300.0;
 
     @FXML
     public void initialize() {
         ObservableList<Integer> availableChoices = FXCollections.observableArrayList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
         choiceBox.setItems(availableChoices);
-        choiceBox.setValue(5);  // Default grid size
-        resize();  // Create the initial default grid
+        choiceBox.setValue(5);
+        grid.setPrefHeight(gridHeight);
+        grid.setPrefWidth(gridWidth);
+        resize();
 
-        // Setup button event handler
         myButton.setOnAction(e -> {
             StringBuilder builder = new StringBuilder();
             for (int[] row : gridData)
@@ -52,31 +44,36 @@ public class Controller {
     }
 
     public void resize() {
-        grid.getChildren().clear();  // Clear the grid first
-        int gridSize = choiceBox.getValue();
-        gridData = new int[gridSize][gridSize];
+        grid.getChildren().clear();
+        int gridSizeHeight = choiceBox.getValue();
+        int gridSizeWidth = choiceBox.getValue();
+        gridData = new int[gridSizeHeight][gridSizeWidth];
+       double rectangleWidth = gridWidth / gridSizeWidth;
+       double rectangleHeight = gridHeight / gridSizeHeight;
 
-        for (int i = 0; i < gridSize; i++) {
-            for (int j = 0; j < gridSize; j++) {
-                Rectangle rect = new Rectangle(30, 30, Color.WHITE);
-//                rect.setStroke(Color.BLACK);
-
-                // Cache in the lambda expression i-th and j-th cell indexes.
-                final int fi = i;
-                final int fj = j;
-
-                rect.setOnMouseClicked(e -> {
-                    if (rect.getFill().equals(Color.WHITE)) {
-                        rect.setFill(Color.BLACK);
-                        gridData[fi][fj] = 1;
-                    } else {
-                        rect.setFill(Color.WHITE);
-                        gridData[fi][fj] = 0;
-                    }
-                });
+        for (int i = 0; i < gridSizeHeight; i++) {
+            for (int j = 0; j < gridSizeWidth; j++) {
+                Rectangle rect = getRectangle(i, j, rectangleWidth, rectangleHeight);
 
                 grid.add(rect, j, i);
             }
         }
     }
+
+    private Rectangle getRectangle(int i, int j, double rectangleWidth, double rectangleHeight) {
+        Rectangle rect = new Rectangle(rectangleWidth, rectangleHeight, Color.WHITE);
+
+        rect.setOnMouseClicked(e -> {
+            if (rect.getFill().equals(Color.WHITE)) {
+                rect.setFill(Color.BLACK);
+                gridData[i][j] = 1;
+            } else {
+                rect.setFill(Color.WHITE);
+                gridData[i][j] = 0;
+            }
+        });
+        return rect;
+    }
+
+
 }
--- src/main/java/com/example/neuralnetworks/NeuralNetworkApplication.java
+++ src/main/java/com/example/neuralnetworks/NeuralNetworkApplication.java
@@ -13,7 +13,7 @@ public class NeuralNetworkApplication extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(NeuralNetworkApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 640, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 700, 300);
         stage.setTitle("NeuralNetwork");
         stage.setScene(scene);
         stage.show();
